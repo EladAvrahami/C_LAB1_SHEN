ddef62973715340c553885e1e7e84b5f7ff2a199
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2024-12-02 02:39:17 +0200

    function that gets params

---
+++ HW4functions/exe3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+//calc two points distance -function that gets params
+void dist_scan(float x1,float y1,float x2,float y2){
+    float distance;
+    distance=sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("The distance between the points is: %.2f\n", distance);
+}
+
+
+int main(){
+    float x1,y1,x2,y2,distance;
+    printf("enter x and y of 2 points each parameter separated by enter:\n");
+    scanf("%f%f%f%f",&x1,&y1,&x2,&y2);
+dist_scan(x1,y1,x2,y2);
+return 0;
+}
