1c64abe681f15f2964061cc56d0619858a781b2b
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2025-01-27 21:55:39 +0200

    strcat function - concatenate two strings

---
+++ class9EXEpointerAndDmemorey/3strcatFunction.c
@@ -0,0 +1,20 @@
+#include<string.h>
+#include<stdio.h>
+
+
+//strcat - concatenate two strings
+//strcat(s1,s2) - concatenates string s2 onto the end of string s1
+//s1 - destination string
+//s2 - source string
+//returns s1
+//strcat(s1,s2) - concatenates string s2 onto the end of string s1
+//need to make sure s1 has enough space to hold the result
+
+int main(){
+
+
+
+
+
+    return 0;
+}
