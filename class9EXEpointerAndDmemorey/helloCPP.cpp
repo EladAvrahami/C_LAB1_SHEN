e3daad958844035f38a25859f4aa57deda4b879a
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2025-01-29 11:54:43 +0200

    test CPP

---
+++ class9EXEpointerAndDmemorey/helloCPP.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//liborary (contain cout&endl opptions)
+using namespace std;//directive as default
+
+
+int main() {
+	cout << "hello world with std declaration on top of the file" << endl;
+// namespace  variable    "string literal constant"
+//	std::      cout          << "hello Wrold " <<           std::endl;
+	return 0;
+}
